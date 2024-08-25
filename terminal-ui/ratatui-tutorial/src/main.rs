fdf0009e365f71f9b99b1aa0f107373a41add91b
Galus <mgalus@gmail.com>
2024-08-25 11:13:09 -0400

    Added ratatui hello world initial code

--- terminal-ui/ratatui-tutorial/src/main.rs
+++ terminal-ui/ratatui-tutorial/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Result};
+
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        event::{self, KeyCode, KeyEventKind},
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+        ExecutableCommand,
+    },
+    style::Stylize,
+    widgets::Paragraph,
+    Terminal,
+};
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
+
+    loop {
+        // draw ui
+        let _ = terminal.draw(|frame| {
+            let area = frame.area();
+            frame.render_widget(
+                Paragraph::new("Hello Ratatui! q - quit").white().on_blue(),
+                area,
+            );
+        });
+
+        // handle events
+        // 16ms ~= 60fps
+        /*
+                quick maths:
+                16ms/frame * 60frames/s = 960 ms
+                17ms/frame * 60frames/s = 1020 ms
+                1000ms / 60frames = 16.66ms/frame
+        */
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
