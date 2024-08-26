2e3f50226bf418f8ff6a5f5fb68ef68cb2d55cc2
Galus <mgalus@gmail.com>
2024-08-25 22:45:10 -0400

    Got the canvas to print a map. Lol

--- terminal-ui/ratatui-canvas/src/main.rs
+++ terminal-ui/ratatui-canvas/src/main.rs
@@ -1,3 +1,151 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::{
+    eyre::{bail, WrapErr},
+    Result,
+};
+
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Alignment, Rect},
+    style::{Color, Stylize},
+    symbols::border,
+    text::{Line as TextLine, Text},
+    widgets::{
+        block::{Position, Title},
+        canvas::{Canvas, Line, Map, MapResolution, Rectangle},
+        Block, Paragraph, Widget,
+    },
+    Frame,
+};
+
+mod tui;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let mut terminal = tui::init()?;
+
+    let app_result = App::default().run(&mut terminal);
+
+    if let Err(err) = tui::restore() {
+        eprintln!(
+            "failed to restore terminal. Run `reset` or restart your terminal to recover: {}",
+            err
+        );
+    }
+    app_result
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut tui::Tui) -> Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events().wrap_err("handle events failed")?;
+        }
+        Ok(())
+    }
+
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => self
+                .handle_key_event(key_event)
+                .wrap_err_with(|| format!("handling key event failed:\n{key_event:#?}")),
+            _ => Ok(()),
+        }
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter()?,
+            KeyCode::Right => self.increment_counter()?,
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    // galus: There is an overflow bug here left for educational porpoises 🎓 🐬
+    fn increment_counter(&mut self) -> Result<()> {
+        self.counter += 1;
+        if self.counter > 2 {
+            bail!("counter overflow");
+        }
+        Ok(())
+    }
+
+    fn decrement_counter(&mut self) -> Result<()> {
+        self.counter -= 1;
+        Ok(())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Counter App Ratatui Tutorial <3 from Galus ".bold());
+        let instructions = Title::from(TextLine::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::THICK); // galus: All caps made me smile THICC
+
+        let counter_text = Text::from(vec![TextLine::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text).centered().block(block);
+        //.render(area, buf);
+
+        let canvas = Canvas::default()
+            .block(Block::bordered().title("Canvas"))
+            .x_bounds([-180.0, 180.0])
+            .y_bounds([-90.0, 90.0])
+            .paint(|ctx| {
+                ctx.draw(&Map {
+                    resolution: MapResolution::High,
+                    color: Color::White,
+                });
+                ctx.layer();
+                ctx.draw(&Line {
+                    x1: 0.0,
+                    y1: 10.0,
+                    x2: 10.0,
+                    y2: 10.0,
+                    color: Color::White,
+                });
+                ctx.draw(&Rectangle {
+                    x: 10.0,
+                    y: 20.0,
+                    width: 10.0,
+                    height: 10.0,
+                    color: Color::Red,
+                });
+            });
+        canvas.render(area, buf)
+    }
 }
